ff04d06c39e0130e5ec34eab290e01b6337d197f
independnt <gonzalezdennis6@gmail.com>
2018-02-21 10:43:39 -0500

    added helper methods to application controller

--- app/controller/user_controller.rb
+++ app/controller/user_controller.rb
@@ -5,4 +5,17 @@ class UserController < ApplicationController
     erb :'users/create_user'
   end
 
+  helper do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+
+  end
+
 end
